dbb03b7beed97b9997618d525053c3a4cd137546
wangke <cn_wk@hotmail.com>
2016-09-25 20:15:18 +0800

    modify bayes

--- bin/bayes.py
+++ bin/bayes.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from numpy import *
 
 
-def trainNB0(trainMatrix, trainCategory):
-    from numpy import *
-    numTrainDocs = len(trainMatrix)
-    numWords = len(trainMatrix[0])
-    pAbusive = sum(trainCategory) / float(numTrainDocs)
-    p0Num, p1Num = zeros(numWords),zeros(numWords)
-    p0Denom, p1Denom = 0.0, 0.0
-    for i in range(numTrainDocs):
-        if 1==trainCategory[i]:
-            p1Num += trainMatrix[i]
-            p1Denom += sum(trainMatrix[i])
-        else:
-            p0Num += trainMatrix[i]
-            p0Denom += sum(trainMatrix[i])
-    p1Vect, p0Vect = p1Num /p1Denom, p0Num /p0Denom
-    return p0Vect, p1Vect, pAbusive
-
+# step1: load data
 def loadDataSet():
     postingList =[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -29,7 +14,6 @@ def loadDataSet():
     classVec = [0, 1, 0, 1, 0, 1]   # 0=正常言论 1=侮辱性文字
     return postingList, classVec
 
-
 def createVocabList(dataSet):
     retVec = set()
     for document in dataSet:
@@ -37,6 +21,7 @@ def createVocabList(dataSet):
     return list(retVec)
 
 
+# step2: convert words to vector
 def setOfWords2Vec(vecList, dataInput):
     ret = [0] * len(vecList)
     for word in dataInput:
@@ -46,13 +31,57 @@ def setOfWords2Vec(vecList, dataInput):
             print "the word %s is not in my Vocabulary !" %(word,)
     return ret
 
-if '__main__' == __name__:
+
+# step3: train
+def trainNB0(trainMatrix, trainCategory):
+
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+
+    p0Num, p1Num = ones(numWords), ones(numWords)
+    p0Denom, p1Denom = 2.0, 2.0
+
+    for i in range(numTrainDocs):
+        if 1==trainCategory[i]:
+            p1Num += trainMatrix[i]   # 分子: 拼接向量
+            p1Denom += sum(trainMatrix[i])   # 分母: 求和
+        else:
+            p0Num += trainMatrix[i]   # 分子: 拼接向量
+            p0Denom += sum(trainMatrix[i])   # 分母: 求和
+
+    p1Vect, p0Vect = log(p1Num /p1Denom), log(p0Num /p0Denom)
+
+    return p0Vect, p1Vect, pAbusive
+
+
+# step4: classify
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1 - pClass1)
+    return 1 if p1 > p0 else 0
+
+
+# step5: test
+def testingNB():
     listOPosts, listClasses = loadDataSet()
     myVocabList = createVocabList(listOPosts)
+
     trainMat = []
     for postinDoc in listOPosts:
         trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
 
-    p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
 
-    print p0V,'\n', p1V, '\n', pAb
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as :', classifyNB(thisDoc, p0V, p1V, pAb)
+
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classifed as : ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+
+if '__main__' == __name__:
+    testingNB()
